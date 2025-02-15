9d3d043c503824a22427b47a4bf8e815ef35244c
ProgAD <aditya012official@gmail.com>
2025-02-15 23:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 async function generateLetter() {
     const name = document.getElementById("name").value || "Mere Mitr";
     const mood = document.getElementById("mood").value;
-    q k mere pass power hai to mai project destroy kar dunga
     const senderName = "Programmer_AD"; // Sender name
     const apiKey = "AIzaSyCm39M8zTTYKS_WVsWYrKa0Ikgq54ElSA4"; // Replace with a valid API key
 
